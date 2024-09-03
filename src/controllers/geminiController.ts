a0f1e7451dd46646d8d0726d0d5153fd36c72bfd
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-09-03 01:06:07 -0300

    adding context to the user prompt for improve the response of the AI and parsing the response to JSON

--- src/controllers/geminiController.ts
+++ src/controllers/geminiController.ts
@@ -1,18 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { generateDatasetParameters } from "@/services/geminiService";
+import { promptContext } from "@/prompts/promptContext";
 
 const createDatasetParameters = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   try {
-    const { modelDescription } = req.body;
-    const promptContext = "El siguiente texto es una descripción de un dataset que sera usado para entrenar un modelo de machine learning";
-    const prompt = modelDescription.toString();
+    const { modelDescriptionPrompt } = req.body;
+
+    const prompt = promptContext + modelDescriptionPrompt.toString();
     const data = await generateDatasetParameters(prompt);
-    res.status(201).json({ success: true, data });
+
+    // Asegúrate de que el string `data` sea un JSON válido
+    let jsonString = data.trim();
+
+    // Si el string está rodeado de comillas simples, conviértelas en comillas dobles
+    if (
+      (jsonString.startsWith("'") && jsonString.endsWith("'")) ||
+      (jsonString.startsWith("```json") && jsonString.endsWith("```"))
+    ) {
+      jsonString = jsonString.slice(1, -1).replace(/'/g, '"');
+    }
+
+    // Parsear el JSON
+    const result = JSON.parse(jsonString);
+    res.status(201).json({ success: true, result });
   } catch (error) {
-    res.status(500).json({ success: false, error });
+    res.status(500).json({ success: false, error: (error as any).message });
   }
 };
 
